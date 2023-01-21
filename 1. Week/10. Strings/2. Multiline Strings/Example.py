73c895e0b350e530eb65eb784b1820cb5dca9744
Amanzhol <69354535+1ncrease@users.noreply.github.com>
2023-01-21 16:41:05 +0600

    test
    test

---
+++ 1. Week/10. Strings/2. Multiline Strings/Example.py
@@ -0,0 +1,16 @@
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+#Or three single quotes:
+
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+
+
+#You can assign a multiline string to a variable by using three quotes:
