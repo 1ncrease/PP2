eee605d3d7b2f0c01d8f3f2d1dcf11cb068361ed
Amanzhol <69354535+1ncrease@users.noreply.github.com>
2023-01-21 12:54:21 +0600

    test
    test

---
+++ 1. Week/3. Get Started/Example 3.py
@@ -0,0 +1,5 @@
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+#The number of spaces is up to you as a programmer, the most common use is four, but it has to be at least one.
