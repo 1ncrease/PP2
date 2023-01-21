81c99375017dff9a8e0b9b1ac3637c7ca13c3d75
Amanzhol <69354535+1ncrease@users.noreply.github.com>
2023-01-21 13:52:30 +0600

    test
    test

---
+++ 1. Week/8. Numbers/1. Int, integer/Integers.py
@@ -0,0 +1,13 @@
+#Floats:
+
+
+x = 1.10
+y = 1.0
+z = -35.59
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#Float, or "floating point number" is a number, positive or negative, containing one or more decimals.
+
---
+++ 1. Week/8. Numbers/3. Complex/Complex Numbers.py
@@ -0,0 +1,13 @@
+#Complex:
+
+
+x = 3+5j
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#Complex numbers are written with a "j" as the imaginary part:
+
