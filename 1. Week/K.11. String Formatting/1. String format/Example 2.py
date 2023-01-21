a9c191226c3aae6c491a7f527bd1e5bfe9dd3ca5
Amanzhol <69354535+1ncrease@users.noreply.github.com>
2023-01-21 17:02:58 +0600

    test
    test

---
+++ 1. Week/K.11. String Formatting/1. String format/Example 2.py
@@ -0,0 +1,5 @@
+#You can add parameters inside the curly brackets to specify how to convert the value:
+
+txt = "The price is {:.2f} dollars"
+
+#Format the price to be displayed as a number with two decimals:
