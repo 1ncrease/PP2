f19d8c3cb1f1513682b5d1b4bc23860f0605f4f7
Amanzhol <69354535+1ncrease@users.noreply.github.com>
2023-01-26 14:44:34 +0600

    test
    test

---
+++ 1. Week/M.13. Operators/Python Comparison Operators/Example.py
@@ -0,0 +1,8 @@
+x, y = int(input()), int(input())
+
+x == y #Equal
+x != y #Not equal
+x > y #x greater than y
+x < y #x less than y
+x >= y #x greater than y or equal to y
+x <= y #x less than y or equal to y
