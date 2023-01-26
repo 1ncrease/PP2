ab021a2002424e65be6932921255582ab82582ee
Amanzhol <69354535+1ncrease@users.noreply.github.com>
2023-01-26 14:51:18 +0600

    test
    test

---
+++ 1. Week/M.13. Operators/Bitwise Operators/Example.py
@@ -0,0 +1,5 @@
+x, y = int(input()), int(input())
+
+x & y #AND Sets each bit to 1 if both bits are 1
+x | y # OR Sets each bit to 1 if one of two bits is 1
+x ^	y # XOR Sets each bit to 1 if only one of two bits is 1
