24ba51fe08814c981310088991c1099c4b114370
Amanzhol <69354535+1ncrease@users.noreply.github.com>
2023-01-26 15:04:02 +0600

    test
    test

---
+++ 1. Week/N.14. If...Else/Short Hand If.py
@@ -0,0 +1,6 @@
+#If you have only one statement to execute, you can put it on the same line as the if statement.
+#One line if statement:
+
+a, b = int(input()), int(input())
+if a > b: print("a is greater than b") 
+
