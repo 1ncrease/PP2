22d650c8110ce02358c6761b3cf9555618e6cf88
Amanzhol <69354535+1ncrease@users.noreply.github.com>
2023-01-28 12:05:24 +0600

    week2
    week2

---
+++ 2. Week/2. Lists/5. Type/Example.py
@@ -0,0 +1,8 @@
+#From Python's perspective, lists are defined as objects with the data type 'list':
+#<class 'list'>
+
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+
+#What is the data type of a list?
+
