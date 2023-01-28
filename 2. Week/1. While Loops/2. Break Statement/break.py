a99cf27b0aae7bdc9a3a2001ec74e29e162ec96c
Amanzhol <69354535+1ncrease@users.noreply.github.com>
2023-01-28 11:55:34 +0600

    week2
    week2

---
+++ 2. Week/1. While Loops/2. Break Statement/break.py
@@ -0,0 +1,10 @@
+#With the break statement we can stop the loop even if the while condition is true:
+
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+
+#Exit the loop when i is 3:
