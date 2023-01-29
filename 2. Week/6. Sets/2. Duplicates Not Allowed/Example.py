9e51c5635e3236e13f335b17ab4a4aa84cb0277e
Amanzhol <69354535+1ncrease@users.noreply.github.com>
2023-01-29 13:24:17 +0600

    week2
    week2

---
+++ 2. Week/6. Sets/2. Duplicates Not Allowed/Example.py
@@ -0,0 +1,7 @@
+#Sets cannot have two items with the same value.
+
+thisset = {"apple", "banana", "cherry", "apple"}
+print(thisset)
+
+#Duplicate values will be ignored:
+
---
+++ 2. Week/6. Sets/5. Type/Example.py
@@ -0,0 +1,8 @@
+#From Python's perspective, sets are defined as objects with the data type 'set':
+#<class 'set'>
+
+myset = {"apple", "banana", "cherry"}
+print(type(myset))
+
+#What is the data type of a set?
+
