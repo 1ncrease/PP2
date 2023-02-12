b75cb5a891dc4cefe3e5ade95bed9a896b0e85d2
Amanzhol <69354535+1ncrease@users.noreply.github.com>
2023-02-12 19:50:37 +0600

    week3
    week3

--- 3. Week/4. Inheritance/2. Create a Child Class/Now the Student class has the same properties and methods as the Person class/Example.py
+++ 3. Week/4. Inheritance/2. Create a Child Class/Now the Student class has the same properties and methods as the Person class/Example.py
@@ -1,18 +1,5 @@
-#Use the Student class to create an object, and then execute the printname method:
+def yadaun(x):
+    print(x, "da ti daun")
 
-class Person:
-  def __init__(self, fname, lname):
-    self.firstname = fname
-    self.lastname = lname
-
-  def printname(self):
-    print(self.firstname, self.lastname)
-
-class Student(Person):
-  pass
-
-x = Person("John", "Doe")
-x.printname()
-
-x = Student("Mike", "Olsen")
-x.printname()
+name = input()
+yadaun(name)
---
+++ 3. Week/Solved tasks/Functions/Example 3.py
@@ -0,0 +1,3 @@
+#Execute a function named my_function.
+def my_function(fname, lname):
+    print(fname)
